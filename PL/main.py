b8c1de225f364cf339ca3388e31b621327311337
Romiz <119493658+Rom1z@users.noreply.github.com>
2026-03-16 22:44:35 +0300

    PL

---
+++ PL/main.py
@@ -0,0 +1,43 @@
+import tkinter as tk
+from main import load_materials
+from tkinter import ttk
+
+import _mysql_connector
+
+
+def connect():
+    return _mysql_connector.connect(
+        host="localhost",
+        user="root",
+        password="12543hRGB2001",
+        database="marupov"
+    )
+def load():
+    for i in tree.get_children():
+        tree.delete(i)
+        conn = connect_db()
+        cursor = conn.cursor()
+        cursor.execute("""
+        select * from marupov"
+                       """)
+
+    for row in cur:
+        tree.insert('', 'end', values=row)
+
+        conn.close()
+
+
+root = tk.Tk()
+root.title("экзамен")
+
+columns = ("ID", "Название", "Тип", "Количество","Цена")
+tree = ttk.Treeview(root, columns = columns, show = "headings")
+
+for column in columns:
+    tree.heading(column, text=column)
+    tree.column(column, width=100)
+
+tree.pack(fill="both", expand=True)
+tk.button(root, text="Обновить", command=load_materials).pack()
+load(); root.mainloop()
+root.mainloop()
---
+++ PL/main2.py
@@ -0,0 +1,92 @@
+import tkinter as tk
+from tkinter import ttk
+import mysql.connector
+
+def connect_db():
+    connection = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="12543hRGB2001",   # укажи свой пароль
+        database="marupov"
+    )
+    return connection
+
+
+
+
+def load_materials():
+
+    for row in tree.get_children():
+        tree.delete(row)
+
+    conn = connect_db()
+    cursor = conn.cursor()
+
+    query = """
+    SELECT 
+    m.id,
+    m.name,
+    mt.name,
+    m.stock_quantity,
+    m.min_quantity
+    FROM materials m
+    JOIN material_types mt
+    ON mt.id = m.type_id
+    """
+
+    cursor.execute(query)
+
+    for row in cursor.fetchall():
+        tree.insert("", tk.END, values=row)
+
+    conn.close()
+
+root = tk.Tk()
+root.title("Материалы на складе")
+root.geometry("800x500")
+root.configure(bg="#FFFFFF")
+
+# Заголовок
+
+title = tk.Label(
+    root,
+    text="Склад материалов",
+    font=("Constantia", 18),
+    bg="#FFFFFF",
+    fg="#405C73"
+)
+title.pack(pady=10)
+
+
+
+columns = ("ID", "Название", "Тип", "Количество", "Минимум")
+
+tree = ttk.Treeview(root, columns=columns, show="headings")
+
+for col in columns:
+    tree.heading(col, text=col)
+    tree.column(col, width=150)
+
+tree.pack(expand=True, fill="both", padx=20, pady=20)
+
+
+
+btn_frame = tk.Frame(root, bg="#FFFFFF")
+btn_frame.pack()
+
+refresh_btn = tk.Button(
+    btn_frame,
+    text="Обновить список",
+    font=("Constantia", 12),
+    bg="#405C73",
+    fg="white",
+    command=load_materials
+)
+
+refresh_btn.pack(pady=10)
+
+
+
+load_materials()
+
+root.mainloop()
